b0ccdb57a628601081d6e732a2329f50da9317c4
Anton Vanhoucke <antonvh@gmail.com>
2023-03-14 08:52:52 +0100

    Added shortcodes

--- beekeeper-esp/main.py
+++ beekeeper-esp/main.py
@@ -21,11 +21,29 @@ KEEP_ALIVE = 5 	# Seconds to keep the connection alive after last publish/update
                 # Also used as timeout to detect when the robot goes offline
 client_id = ubinascii.hexlify(machine.unique_id())
 topic_root = b'beekeepers/'+ client_id + b'/'
-topics = {
-    'status' : topic_root+b'status',
-    'command': topic_root+b'command',
-    'response': topic_root+b'response'
-    }
+# topics = {
+#     'status' : topic_root+b'status',
+#     'command': topic_root+b'command',
+#     'response': topic_root+b'response',
+#     'charging': topic_root+b'charging',
+#     'battery': topic_root+b'battery',
+#     }
+
+cmd_shortcodes = {
+    'FW' : ('straight', (100,)),
+    'BK' : ('straight', (-100,)),
+    'WG' : ('wriggle', ()),
+    'TL' : ('straight', (0, -90)),
+    'TR' : ('straight', (0, 90)),
+    'DU' : ('drill_up', ()),
+    'DD' : ('drill_down', ()),
+    'T0' : ('set_tank_level', (0,)),
+    'T5' : ('set_tank_level', (50,)),
+    'T9' : ('set_tank_level', (100,)),
+ }
+
+def topic(name):
+    return topic_root+bytes(name, "UTF-8")
 
 mqtt_client = MQTTClient(client_id, 
                             mqtt_server, 
@@ -34,10 +52,11 @@ mqtt_client = MQTTClient(client_id,
                             mqtt_password,
                             keepalive = KEEP_ALIVE # Seconds
                          ) 
-mqtt_client.set_last_will(topics['status'], b'Offline', retain=True, qos=2)
+mqtt_client.set_last_will(topic('status'), b'Offline', retain=True, qos=2)
 
 command = ""
 args = []
+status = {}
 
 # connect to wifi
 def connect_wifi():
@@ -50,12 +69,14 @@ def connect_wifi():
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
+    st.call('sound_loaded')
     
 
 def sub_callback(topic, msg):
     global command, args
-    print((topic, msg))
-    if topic == topics['command']:
+    # print((topic, msg)) # debug
+    if topic == topic('command'):
+        # Someone posted a command for us, let's parse it.
         if not b"," in msg:
             command = msg.decode("UTF-8")
             args = []
@@ -63,6 +84,10 @@ def sub_callback(topic, msg):
             items = msg.decode("UTF-8").split(",")
             command = items[0]
             args = [ eval(arg) for arg in items[1:] ]
+    if topic == topic('short'):
+        if command in cmd_shortcodes:
+            command = cmd_shortcodes[command][0]
+            args = cmd_shortcodes[command][1]
 
 mqtt_client.set_callback(sub_callback)
 
@@ -70,35 +95,43 @@ def connect_mqtt():
     result = None
     try:
         result = mqtt_client.connect()
-        mqtt_client.publish(topics['status'], b'Ready', retain=True)        
-        mqtt_client.subscribe(topics['command'])
+        mqtt_client.publish(topic('status'), b'Ready', retain=True)        
+        mqtt_client.subscribe(topic('command'))
         
     except OSError as e:
         result = e
     print('Connected to %s MQTT broker with result %s' % (mqtt_server, result))
 
 
+def publish_status():
+    if status['rst']:
+        mqtt_client.publish(topic('status'), b'Resting')
+    else:
+        mqtt_client.publish(topic('status'), b'Ready')
+    mqtt_client.publish(topic('battery'), str(status['bat']))
+    mqtt_client.publish(topic('charging'), str(status['chg']))
+
 
 connect_wifi()
 connect_mqtt()
 last_update = time.ticks_ms()
-count=0
+
 while 1:
     try:
         mqtt_client.check_msg()
         if command:
-            mqtt_client.publish(topics['status'], b'Busy')
-            time.sleep_ms(100)
-            resp = st.call(command, *args, timeout=1500000)
-            mqtt_client.publish(topics['response'], repr(resp)) #repr((command,args)))
-            mqtt_client.publish(topics['status'], b'Ready')
+            # We are receiving a command in our 'command' topic
+            mqtt_client.publish(topic('status'), b'Busy')
+            # time.sleep_ms(100)
+            ack, data = st.call(command, *args, timeout=1500000)
+            if type(data) == dict:
+                status = data
+            # mqtt_client.publish(topics['response'], repr(resp)) #repr((command,args)))
             command = ""
             args = []
             last_update = time.ticks_ms()
         if time.ticks_ms() > last_update + KEEP_ALIVE*900:
-            #mqtt_client.publish(topics['status'], 'Ready %s' % count, retain=True)
-            #count +=1
-            mqtt_client.publish(topics['status'], 'Ready')
+            mqtt_client.publish(topic('status'), 'Ready')
             last_update = time.ticks_ms()
     except OSError as e:
         print("Reconnecting...")
