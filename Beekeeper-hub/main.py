65a1a8aa3ba664d029612cc5ed6bf064bc25379b
Anton Vanhoucke <antonvh@gmail.com>
2023-03-14 08:53:17 +0100

    added resting check & sound

--- Beekeeper-hub/main.py
+++ Beekeeper-hub/main.py
@@ -3,72 +3,97 @@
 from serialtalk import SerialTalk
 from mshub import MSHubSerial
 from pybricks import Direction, Port, ForceSensor, DriveBase, Motor, wait, Stop
+import hub
 
+hub.sound.play('sounds/startup')
+
+# Open comms
 st = SerialTalk(MSHubSerial('F'))
+
+# Define motors and sensors
+wait(600) # Some time to handshake hub object and motors
 switch = ForceSensor(Port.E)
 
 # Init drivebase
 r_track = Motor(Port.A)
 l_track = Motor(Port.C, Direction.COUNTERCLOCKWISE)
-tracks = DriveBase(l_track, r_track, 50, 125)
+tracks = DriveBase(l_track, r_track, 41.5, 160)
 
 # Init drill
 drill = Motor(Port.B, Direction.COUNTERCLOCKWISE)
 
 # Init tanks
 tanks = Motor(Port.D) 
-tanks.run_until_stalled(-200, duty_limit=30) # Run until empty
-tanks.reset_angle(0)
-TANK_FULL = 155
+tanks.run_until_stalled(-200, duty_limit=45) # Run until empty
+tanks.reset_angle(-10) # Add some extra -10 degrees to make sure 0 is without motor tension
+TANK_FULL = 160 # Degrees motor position.
 
 def set_tank_level(pct):
-    pct = min(max(pct,0),100)
+    pct = min(max(pct,0),100) # Cap percentage
     tanks.run_target(50, pct/100 * TANK_FULL, wait=False)
-    
-def get_tank_level():
-    return tanks.angle()*100//TANK_FULL
+set_tank_level(100)
 
+def get_tank_level():
+    # Return a number between 0 and 100, relative to the 
+    # tank motor angle/tank fill level.
+    return min(max( tanks.angle()*100//TANK_FULL, 0), 100)
+
+def resting():
+    return 1 if switch.pressed() else 0
+
+def status():
+    return {
+        'rst': resting(),
+        'bat': hub.battery.capacity_left(),
+        'chg' : hub.battery.charger_detect(),
+        'tnk': get_tank_level(),
+        }
+
+def not_when_resting(func):
+    # Decorator to ensure the robot does not move when in its cradle
+    def cradle_safe_func(*args, **kwargs):
+        if not resting():
+            func(*args, **kwargs)
+        return status()
+    return cradle_safe_func
+
+@not_when_resting    
 def drill_up():
     drill.run_target(200,5,Stop.BRAKE)
 
+@not_when_resting
 def drill_down():
     drill.run_target(200,80,Stop.BRAKE)
 
+@not_when_resting
 def wriggle(n=3, angle=25):
     tracks.curve(0, -angle/2)
     for i in range(n):
         tracks.curve(0, angle)
         tracks.curve(0, -angle)
     tracks.curve(0, angle/2)
-    
-def wait_until(function, condition=True):
-    while not function() == condition:
-        wait(10)
+        
+def sound_loaded():
+    hub.sound.play('sounds/menu_program_start')
 
-# while True:
-#     if switch.pressed():
-#         drill_up()
-#         set_tank_level(100)
-#         wait_until(switch.pressed, False)
-#     else:
-#         wait(1000)
-#         tracks.straight(100)
-#         tracks.curve(0, 90)
-#         drill_down()
-#         set_tank_level(0)
-#         wriggle()
-#         drill_up()
-#         tracks.straight(-100)
-#         wait_until(switch.pressed, True)
+@not_when_resting
+def straight(distance=100):
+    tracks.straight(distance)
+
+@not_when_resting
+def turn(radius=0, angle=90):
+    tracks.curve(radius, angle)
+
 
 st.add_command(get_tank_level,"b")
-st.add_command(set_tank_level)
-st.add_command(wriggle)
-st.add_command(drill_down)
-st.add_command(drill_up)
-st.add_command(tracks.straight, name="straight")
-st.add_command(tracks.curve, name="curve")
-st.add_command(switch.pressed, name="resting")
+st.add_command(set_tank_level, "repr")
+st.add_command(wriggle, "repr")
+st.add_command(drill_down, "repr")
+st.add_command(drill_up, "repr")
+st.add_command(straight, "repr")
+st.add_command(turn, "repr")
+st.add_command(hub.sound.beep, "repr", name="beep")
+st.add_command(sound_loaded, "repr")
+st.add_command(status, "repr")
 st.loop()
-
 
